11b1e7e4c49136c14225840e48a998d33224ae37
akashR011 <Akalathingal4315@conestogac.on.ca>
2023-12-09 14:48:13 -0500

    Final code added with cleaning and combining

---
+++ Project_Group-9.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+# Data Collection
+df1 = pd.read_csv('Original Dataset1/movies.csv')
+df2 = pd.read_csv('Original Dataset2/tv-series.csv')
+
+# Data Integration
+merged_df = pd.merge(df1, df2, on=['id', 'title', 'vote_average', 'status', 'vote_count', 'homepage', 'spoken_languages', 'original_language', 'tagline', 'production_companies', 'production_countries', 'adult', 'backdrop_path', 'genres', 'popularity', 'poster_path', 'overview'], how='outer')
+
+# Data Storage and Maintenance
+# Assuming you want to store the merged data as a new CSV file
+merged_df.to_csv('Merged_Dataset.csv', index=False)
+
+# Combine all columns with '_x' and '_y' suffixes into a single column
+for column in merged_df.columns:
+    if column.endswith('_x'):
+        base_column = column[:-2]
+        merged_df[base_column] = merged_df[column].combine_first(merged_df[f'{base_column}_2'])
+
+# Data Quality Checks and Data Cleaning
+# Assuming you want to drop duplicates
+merged_df = merged_df.drop_duplicates()
+
+# Handling Missing Values
+# Example: Impute missing values for numeric columns with mean, for categorical columns with mode
+numeric_columns = merged_df.select_dtypes(include='number').columns
+categorical_columns = merged_df.select_dtypes(exclude='number').columns
+
+merged_df[numeric_columns] = merged_df[numeric_columns].fillna(merged_df[numeric_columns].mean())
+merged_df[categorical_columns] = merged_df[categorical_columns].fillna(merged_df[categorical_columns].mode().iloc[0])
+
+# Drop the original release_date column
+columns_to_drop = ['number_of_seasons', 'release_date', 'number_of_episodes', 'original_title', 'budget', 'revenue', 'runtime', 'first_air_date', 'last_air_date', 'in_production', 'original_name', 'type', 'created_by', 'languages', 'networks', 'origin_country', 'episode_run_time', 'imdb_id']
+merged_df = merged_df.drop(columns=columns_to_drop, axis=1)
+
+# Find and print columns with missing values
+columns_with_missing_values = merged_df.columns[merged_df.isnull().any()]
+
+# Checking for columns with missing values
+for column in columns_with_missing_values:
+    print(f"{column}: {merged_df[column].isnull().sum()} missing values")
+
+# Display the cleaned and integrated data
+print(merged_df.head())
+
+# Save the combined dataset to a new CSV file
+merged_df.to_csv('Combined Dataset/tmdb_dataset.csv', index=False)
